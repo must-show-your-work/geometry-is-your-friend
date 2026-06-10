420805264e9d44895681b7d5e276ae2d6bae7201
Joe Fredette <jfredett@gmail.com>
2026-06-10 11:25:53 -0400

    syntax, glorious syntax

--- Geometry/Ch3/Prop/P7.lean
+++ Geometry/Ch3/Prop/P7.lean
@@ -9,6 +9,7 @@ import Geometry.Theory.Arrangement
 import Geometry.Theory.Forgetting
 import Geometry.Theory.Distinct
 import Geometry.Theory.Collinear
+import Geometry.Theory.Angle
 
 import Geometry.Tactics
 
@@ -26,40 +27,18 @@ open Geometry.Theory
 open Geometry.Ch3.Prop
 open Atlas
 
-
--- p18 'Rays AB and AC are "opposite" if they are distinct, if they emanate from the same point A, and if they are part
--- of the same line AB = AC'
-def OppositeRay (A B C : Point) : Prop :=
-  (ray A B : Set Point) ≠ ray A C ∧ (line A B : Set Point) = line A C
-
--- TODO: Syntax `ray A B is opposite ray A C` -- only applies to rays I think.
-
--- p18, 'An "angle with vertex A" is a point A together with distinct, non-opposite rays AB and AC (called the _sides_
--- of the angle) emanating from A (see figure 1.7)[^9]
---
--- We use the notation ∠ A, ∠ BAC, or ∠ CAB for this angle. If r = ray A B and s = ray A C, then rays r, s are said to
--- be coterminal (meaning they emanate from the same vertex), and the angle is also denoted ∠(r, s).
---
--- [footnote 9] According to this definition, there is no such thing in our treatment as a "straight angle", nor is
--- there such a thing as a "zero angle." We eliminated those expressions because most of the assertions we will make
--- about angles do not apply to them.'
-@[reducible] def Angle (A B C : Point) : Prop :=
-  distinct A B C ∧ ¬OppositeRay A B C
-
--- FIXME: How do I _just_ talk about rays in my model? Some kind of 'abstract ray' type?
--- def Coterminal (r s : Ray) : Prop := ∃ A B C : Point, A on r ∧ B on r ∧ A on s ∧ C on s ∧ ¬OppositeRay r s
---
--- FIXME: not sure if this covers what I need, can't refer by handing it a whole ray, for sure. hm.
-def Coterminal (A B C : Point) : Prop :=
-  ¬ OppositeRay A B C ∧ (ray A B : Set Point) ≠ ray A C
-  
--- TODO: Syntax ∠ B A C -> Angle B A C = Angle C A B; ∠(r,s) also needs support. ∠ (ray A B) (ray A C).
-  
 -- p115, 'Given an angle ∠ CAB, define a point D to be in the interior of ∠CAB if D is on the same side of AC as B and
 -- if D is also on the same side of AB as C. (Thus, the interior of an angle is the intersection of two half-planes.)
 -- See Figure 3.11.'
 def InteriorOf (B A C D : Point) :=
-  Angle B A C ∧ (ray A C guards B and D) ∧ (ray A B guards C and D)
+  ∠ B A C ∧ (ray A C guards B and D) ∧ (ray A B guards C and D)
+
+syntax (name := isInteriorToAngleNotation)
+  ident "is" "interior" "to" "∠" ident ident ident : term
+
+macro_rules (kind := isInteriorToAngleNotation)
+  | `($D:ident is interior to ∠ $X:ident $V:ident $Z:ident) =>
+      `(InteriorOf $X $V $Z $D)
 
 atlas commentary := by
   via proposition 3.7
@@ -90,8 +69,8 @@ atlas commentary := by
 
 
 atlas proposition 3.7 "D on line BC is interior to ∠CAB iff B-D-C"
-  { A B C D : Point } (DonBC : D on line B C) (aCAB : Angle C A B := by assumption) (DonBC : D on line B C := by assumption) :
-    InteriorOf B A C D ↔ B-D-C := by
+  { A B C D : Point } (DonBC : D on line B C) (aCAB : ∠ C A B := by assumption) (DonBC : D on line B C := by assumption) :
+    D is interior to ∠ B A C ↔ B-D-C := by
     have cBCD : collinear B C D := by
       use line B C
       intro P PisBCD; by_exhaustion PisBCD
