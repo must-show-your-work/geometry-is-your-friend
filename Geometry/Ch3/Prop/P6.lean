ead98276e9b98e09246a3a2b9c9a55857191bce4
Joe Fredette <jfredett@gmail.com>
2026-06-12 14:30:20 -0400

    finish 3.6 with new organize! tactic and friends

--- Geometry/Ch3/Prop/P6.lean
+++ Geometry/Ch3/Prop/P6.lean
@@ -32,8 +32,6 @@ atlas commentary := by
       infer
     }
 
-
-
 atlas proposition 3.6 "If A - B - C, then B cuts line AC into two parts"
   { A B C : Point } (ABC : A - B - C := by assumption) :
   ((ray B A) intersects (ray B C) at B) ∧ ((ray A B : Set Point) = (ray A C)) := by
@@ -43,21 +41,13 @@ atlas proposition 3.6 "If A - B - C, then B cuts line AC into two parts"
     · intro P ⟨PinBA, PinBC⟩
       fixme "This is _misery_, some kind of table-based approach is what's needed, 'here are all the cases in a matrix,
 fill them in' kind of thing"
-      todo "Additionally, we need something like a 'find all possible valid arrangments under the current assumption for
-all collinear points'"
       rcases PinBA with PinSegBA | PinExtBA
       · rcases PinBC with PinSegBC | PinExtBC
         · rcases PinSegBA with BPA | rfl | rfl
           · exfalso
             rcases PinSegBC with BPC | rfl | rfl
             · idea "this essentially argues that the points are arranged A - P - B - P - C; which is absurd"
-              fixme "I keep hitting a 'cycle in constraints' which is true, except that's the point, the fact that it
-              found the cycle tells me that I can derive false. In particular it's noting precisly the idea above, P < B < P is
-              absurd. I think the topoSort needs to detect and return the 'I found a cycle you have archisplosion
-              happening' maybe something like an `absurd arrangement` tactic?"
-              have : A - B - P - C := by organize ABC BPC
-              have : A - B - P := by arrangement this
-              exact via lemma 1.0.19 ⟨BPA.symm, this⟩
+              exact via lemma 1.0.19 ⟨BPA.symm, by organize! ABC BPC⟩
             · exact via lemma 1.0.18 ⟨BPA, BPA⟩
             · exact via lemma 1.0.20 ⟨BPA, ABC⟩
           · obvious
@@ -67,54 +57,44 @@ all collinear points'"
             all_goals separate at dABC; contradiction
         obtain ⟨BCP, PneB, PneC⟩ := PinExtBC
         · rcases PinSegBA with BPA | rfl | rfl
-          · exfalso
-            have : A - P - B - C := by organize BPA.symm ABC
-            have : P - B - C := by arrangement this
-            exact via lemma 1.0.19 ⟨this, BCP.symm⟩
+          · exfalso; exact via lemma 1.0.19 ⟨by organize! BPA.symm ABC, BCP.symm⟩
           · obvious
-          · exfalso
-            exact via lemma 1.0.19 ⟨ABC, BCP.symm⟩
+          · exfalso; exact via lemma 1.0.19 ⟨ABC, BCP.symm⟩
       · obtain ⟨BAP, PneB, PneA⟩ := PinExtBA
         rcases PinBC with PinSegBC | PinExtBC
         · rcases PinSegBC with BPC | rfl | rfl
-          · exfalso
-            have : P - A - B - C := by organize ABC BAP.symm
-            have : P - B - C := by arrangement this
-            exact via lemma 1.0.20 ⟨this, BPC.symm⟩
+          · exfalso; exact via lemma 1.0.20 ⟨by organize! ABC BAP.symm, BPC.symm⟩
           · obvious
-          · exfalso
-            exact via lemma 1.0.18 ⟨BAP, ABC⟩
+          · exfalso; exact via lemma 1.0.18 ⟨BAP, ABC⟩
         · exfalso
           obtain ⟨BCP, PneB, PneC⟩ := PinExtBC
-          have : P - A - B - C := by organize BAP.symm ABC
-          have : P - B - C := by arrangement this
-          exact via lemma 1.0.19 ⟨this, BCP.symm⟩
+          exact via lemma 1.0.19 ⟨by organize! BAP.symm ABC, BCP.symm⟩
     · intro P PisB
       obvious
   · apply Subset.antisymm
     · intro P PinAB
       rcases PinAB with (APB | rfl | rfl) | ⟨ABP, AneP, BneP⟩
-      · comment "I regard applications of 3.3 and its friends as obvious; so does the author after this theorem, more or less"
-        have : A - P - B - C := obvious
-        have : A - P - C := by arrangement this
-        have : P on ray A C := obvious
+      · comment "I regard applications of 3.3 and its friends as obvious via this `organize!` tactic; so does the author after this theorem, more or less"
+        have : P on ray A C := by
+          organize! APB ABC
+          have : A - P - C := obvious; obvious
         exact this
       · obvious
       · obvious
-      · have : A - B - C - P ∨ A - B - P - C := by sorry
-        rcases this with ABCP | ABPC
-        · have : A - C - P := by arrangement ABCP
-          obvious
+      · clearly P ≠ C
+        rcases (by organize! ABP ABC PneC) with ABPC | ABCP
         · have : A - P - C := by arrangement ABPC
+          obvious
+        · have : A - C - P := by arrangement ABCP
           obvious
     · intro P PinAC
       rcases PinAC with PinSegAC | PinExtAC
       · rcases PinSegAC with APC | rfl | rfl
-        · have : A - P - B - C ∨  A - B - P - C := by sorry
-          rcases this with APBC | ABPC
-          · have : A - P - B := by arrangement APBC
-            obvious
+        · clearly P ≠ B
+          rcases (by organize! APC ABC PneB) with ABPC | APBC
           · have : A - B - P := by arrangement ABPC
+            obvious
+          · have : A - P - B := by arrangement APBC
             obvious
         all_goals obvious
       · obtain ⟨ACP, PneA, PneC⟩ := PinExtAC
