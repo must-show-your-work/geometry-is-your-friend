b46a1246b1d48b539757b0336725b3346025e183
Joe Fredette <jfredett@gmail.com>
2026-06-12 16:41:16 -0400

    progress on p7, add missing distinctness cond to angle

--- Geometry/Ch3/Prop/P7.lean
+++ Geometry/Ch3/Prop/P7.lean
@@ -27,11 +27,11 @@ open Geometry.Theory
 open Geometry.Ch3.Prop
 open Atlas
 
--- p115, 'Given an angle ∠ CAB, define a point D to be in the interior of ∠CAB if D is on the same side of AC as B and
--- if D is also on the same side of AB as C. (Thus, the interior of an angle is the intersection of two half-planes.)
+-- p115, 'Given an angle ∠ CAB, define a point D to be in the interior of ∠CAB if D is on the same side of line AC as B and
+-- if D is also on the same side of line AB as C. (Thus, the interior of an angle is the intersection of two half-planes.)
 -- See Figure 3.11.'
 def InteriorOf (B A C D : Point) :=
-  ∠ B A C ∧ (ray A C guards B and D) ∧ (ray A B guards C and D)
+  ∠ B A C ∧ (line A C guards B and D) ∧ (line A B guards C and D)
 
 syntax (name := isInteriorToAngleNotation)
   ident "is" "interior" "to" "∠" ident ident ident : term
@@ -47,9 +47,12 @@ atlas commentary := by
   figure 3.12"
   preface ""
 
-  figure := by
-    construction { infer }
-    title "Proposition 3.7"
+  /- figure := by -/
+  /-   construction { -/
+  /-     infer -/
+  /-     focus A -/
+  /-   } -/
+  /-   title "Proposition 3.7" -/
 
 atlas proposition 3.7 "D on line BC is interior to ∠CAB iff B-D-C"
   { A B C D : Point } (DonBC : D on line B C) (aCAB : ∠ C A B := by assumption) (DonBC : D on line B C := by assumption) :
@@ -58,38 +61,43 @@ atlas proposition 3.7 "D on line BC is interior to ∠CAB iff B-D-C"
       use line B C
       intro P PisBCD; by_exhaustion PisBCD
       all_goals obvious
-    obtain ⟨rABnerAC, _⟩ := aCAB
+    obtain ⟨dABC, rABnerAC, rABnopprAC⟩ := aCAB
     constructor
     · intro DintCAB
       obtain ⟨aBAC, ACguardsBD, ABguardsCD⟩ := DintCAB
-      obtain ⟨BoffAC, DoffAC, guardConditionBD⟩ := ACguardsBD
-      obtain ⟨CoffAB, DoffAB, guardConditionCD⟩ := ABguardsCD
+      have ⟨BoffAC, DoffAC, guardConditionBD⟩ := ACguardsBD
+      have ⟨CoffAB, DoffAB, guardConditionCD⟩ := ABguardsCD
+      clearly A ≠ D := by rw [AeqD] at DoffAB; obvious
+      clearly B ≠ D := by rw [BeqD] at DoffAB; obvious
+      clearly C ≠ D := by rw [CeqD] at DoffAC; obvious
+      have dABCD : distinct A B C D := by
+        separate; distinguish; obvious
       rcases guardConditionBD with rfl | h
       · obvious
       · rcases guardConditionCD with rfl | j
         · exfalso; exact absurd DoffAC obvious
-        · clearly B ≠ D
-          clearly C ≠ D
-          have : distinct B C D := by
-            separate; obvious
-          rcases (via axiom B.3 B C D ⟨this, cBCD⟩) with ⟨BCD, _, _⟩ | ⟨_,CBD,_⟩ | ⟨_, _, BDC⟩
-          · exfalso
-            idea "violates the guard condition"
-            sorry
-          · exfalso
-            idea "ibid"
-            sorry
+        · rcases (via axiom B.3 B C D ⟨dABCD forgetting A, cBCD⟩) with ⟨BCD, _, _⟩ | ⟨_,CBD,_⟩ | ⟨_, _, BDC⟩
+          · exfalso; idea "violates the guard condition"
+            have CoffBD : C on line B D := obvious
+            have : (line A C : Line) ≠ line B D := by intro h; rw [h] at DoffAC; exact DoffAC (by obvious)
+            have : (line A C : Line) intersects line B D at C := by
+              sorry
+            have : line A C splits B and D := via lemma 2.0.22 BCD this
+            exact (absurd ACguardsBD) this
+          · exfalso; idea "ibid"
+            have : B on line C D := obvious
+            have : line A B intersects (segment C D : Line) at B := by sorry
+            have : line A B splits C and D := via lemma 2.0.22 CBD this
+            exact (absurd ABguardsCD) this
           · exact BDC
     · intro BDC
       idea "construct rays DB and DC and use 3.6"
       have ⟨h, j⟩ := via proposition 3.6 BDC
       constructor
       · constructor
-        · exact rABnerAC.symm
-        · by_contra!; obtain ⟨_, BAeqBC⟩ := this
-          -- rw [<- BAeqBC] at DonBC -- why doesn't this work?
-          sorry
-      · suffices key : ∀ {B C : Point}, B - D - C → (ray A C guards B and D) by
+        · sorry
+        · sorry
+      · suffices key : ∀ {B C : Point}, B - D - C → (line A C guards B and D) by
           exact ⟨key BDC, key BDC.symm⟩
         intro B C BDC
         sorry
