a8b4089341ddb67a58945c45b26cb678921ae6ed
Joe Fredette <jfredett@gmail.com>
2026-06-16 01:06:08 -0400

    Theory: export LineV2 + Tangling modules

--- Geometry/Theory.lean
+++ Geometry/Theory.lean
@@ -1,6 +1,8 @@
 import Geometry.Theory.Interpendices.A
 import Geometry.Theory.Distinct
 import Geometry.Theory.Angle
+import Geometry.Theory.LineV2
+import Geometry.Theory.Tangling
 
 import Geometry.Theory.Axioms
 import Geometry.Theory.Ch1
