d97f125c9d6756e4b0f03b4c99a186328265e45f
Joe Fredette <jfredett@gmail.com>
2026-06-16 01:26:38 -0400

    LineV2: Line.mem_def simp lemma

--- Geometry/Theory/LineV2.lean
+++ Geometry/Theory/LineV2.lean
@@ -136,6 +136,8 @@ macro_rules (kind := comparingTac)
 @[simp] theorem mkSegment_rightBound {A B : Point} (h : A ≠ B) :
     (mkSegment A B h).rightBound = some B := rfl
 
+@[simp] theorem Line.mem_def {L : Line} {P : Point} : P ∈ L ↔ L.contains P := Iff.rfl
+
 theorem Line.mem_line {A B P : Point} (h : A ≠ B) :
     P ∈ mkLine A B h ↔ P = A ∨ P = B ∨ A - P - B ∨ A - B - P ∨ P - A - B := by sorry
 theorem Line.mem_ray {A B P : Point} (h : A ≠ B) :
